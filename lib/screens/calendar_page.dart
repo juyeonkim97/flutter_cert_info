40eafc21fa67abdc3a7bca2899e359ed773a7cba
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2023-03-04 21:14:39 +0900

    Revert "cr_calendar state 관련 해결"

--- lib/screens/calendar_page.dart
+++ lib/screens/calendar_page.dart
@@ -108,39 +108,38 @@ class _CalendarPageState extends State<CalendarPage> {
   }
 
   Widget listViewWidget() {
-    return Container(
-        color: Colors.white,
-        child: ListView.builder(
-            itemCount: _scheduleList.length,
-            itemBuilder: (BuildContext context, int index) {
-              final item = _scheduleList[index];
-              return ExpansionTile(
-                textColor: eventColors[0],
-                title: Text(item.getFieldDescription('implSeq'),
-                    style: const TextStyle(
-                        fontSize: 18, fontWeight: FontWeight.w500)),
-                children: <Widget>[
-                  ListTile(
-                      title: Text(
-                          '${item.getFieldDescription('docRegStartDt')} : ${item.docRegStartDt} ~ ${item.docRegEndDt}')),
-                  ListTile(
-                      title: Text(
-                          '${item.getFieldDescription('docExamStartDt')} : ${item.docExamStartDt} ~ ${item.docExamEndDt}')),
-                  ListTile(
-                      title: Text(
-                          '${item.getFieldDescription('docPassDt')} : ${item.docPassDt}')),
-                  ListTile(
-                      title: Text(
-                          '${item.getFieldDescription('pracRegStartDt')} : ${item.pracRegStartDt} ~ ${item.pracRegEndDt}')),
-                  ListTile(
-                      title: Text(
-                          '${item.getFieldDescription('pracExamStartDt')} : ${item.pracExamStartDt} ~ ${item.pracExamEndDt}')),
-                  ListTile(
-                      title: Text(
-                          '${item.getFieldDescription('pracPassDt')} : ${item.pracPassDt}')),
-                ],
-              );
-            }));
+    return ListView.builder(
+        itemCount: _scheduleList.length,
+        itemBuilder: (BuildContext context, int index) {
+          final item = _scheduleList[index];
+          return ExpansionTile(
+            backgroundColor: Colors.white,
+            textColor: eventColors[0],
+            title: Text(item.getFieldDescription('implSeq'),
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+            children: <Widget>[
+              ListTile(
+                  title: Text(
+                      '${item.getFieldDescription('docRegStartDt')} : ${item.docRegStartDt} ~ ${item.docRegEndDt}')),
+              ListTile(
+                  title: Text(
+                      '${item.getFieldDescription('docExamStartDt')} : ${item.docExamStartDt} ~ ${item.docExamEndDt}')),
+              ListTile(
+                  title: Text(
+                      '${item.getFieldDescription('docPassDt')} : ${item.docPassDt}')),
+              ListTile(
+                  title: Text(
+                      '${item.getFieldDescription('pracRegStartDt')} : ${item.pracRegStartDt} ~ ${item.pracRegEndDt}')),
+              ListTile(
+                  title: Text(
+                      '${item.getFieldDescription('pracExamStartDt')} : ${item.pracExamStartDt} ~ ${item.pracExamEndDt}')),
+              ListTile(
+                  title: Text(
+                      '${item.getFieldDescription('pracPassDt')} : ${item.pracPassDt}')),
+            ],
+          );
+        });
   }
 
   Widget calendarWidget() {
