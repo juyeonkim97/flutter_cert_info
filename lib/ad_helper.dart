a88a1a1a5e3bd5e271d719a2a7b84f2ecc40dbb1
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2023-03-10 03:10:01 +0900

    (fix) dotenv.env nullable

--- lib/ad_helper.dart
+++ lib/ad_helper.dart
@@ -1,17 +1,17 @@
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AdHelper {
-
   static String get bannerAdUnitId {
     if (!kReleaseMode) {
-      return dotenv.env['TEST_BANNER_UNIT_ID'];
+      return dotenv.env['TEST_BANNER_UNIT_ID'] ?? '';
     }
     if (Platform.isAndroid) {
-      return dotenv.env['ANDROID_BANNER_UNIT_ID'];
+      return dotenv.env['ANDROID_BANNER_UNIT_ID'] ?? '';
     } else if (Platform.isIOS) {
-      return dotenv.env['IOS_BANNER_UNIT_ID']; // TODO 변경 필요
+      return dotenv.env['IOS_BANNER_UNIT_ID'] ?? ''; // TODO 변경 필요
     } else {
       throw new UnsupportedError('Unsupported platform');
     }
